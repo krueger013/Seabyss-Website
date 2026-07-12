fda820754390d68b2190832d686ca386a654ee8b
krueger013 <krueger013@hotmail.com>
2026-07-12 18:07:43 -0400

    Publish Seabyss Launcher 0.1.3

--- js/config.js
+++ js/config.js
@@ -3,8 +3,8 @@
         apiBaseUrl: "https://api.seabyss.com",
         environment: "beta",
         siteVersion: "0.1.6",
-        launcherVersion: "0.1.2",
-        launcherDownloadUrl: "https://github.com/krueger013/Seabyss-Website/releases/download/launcher-0.1.2/SeabyssLauncher-0.1.2-Setup.zip",
+        launcherVersion: "0.1.3",
+        launcherDownloadUrl: "https://github.com/krueger013/Seabyss-Website/releases/download/launcher-0.1.3/SeabyssLauncher-0.1.3-Setup.zip",
         manifestUrl: "launcher/seabyss_manifest.json"
     };
 })();
