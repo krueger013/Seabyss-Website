338d5cf7ce66f1caa66b760d5136776a934accae
krueger013 <krueger013@hotmail.com>
2026-08-11 09:59:29 -0400

    Publish Seabyss Launcher 0.1.6

--- js/config.js
+++ js/config.js
@@ -3,8 +3,8 @@
         apiBaseUrl: "https://api.seabyss.com",
         environment: "beta",
         siteVersion: "0.1.6",
-        launcherVersion: "0.1.5",
-        launcherDownloadUrl: "https://github.com/krueger013/Seabyss-Website/releases/download/launcher-0.1.5/SeabyssLauncher-0.1.5-Setup.exe",
+        launcherVersion: "0.1.6",
+        launcherDownloadUrl: "https://github.com/krueger013/Seabyss-Website/releases/download/launcher-0.1.6/SeabyssLauncher-0.1.6-Setup.exe",
         manifestUrl: "launcher/v2/seabyss_manifest.json"
     });
 })();
